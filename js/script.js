a71d9b4372704a32566a83e2354d03b074278604
parkermerritt <parker.merritt@gmail.com>
2017-06-05 20:06:59 -0400

    Add spincap js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+$(window).scroll(function() {
+var theta = $(window).scrollTop()/100.0 % (Math.PI*4.5);
+$('#spincap').css({ transform: 'rotate(-' + theta + 'rad)' });
+});
