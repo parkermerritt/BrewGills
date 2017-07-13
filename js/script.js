ad62c72bcab730d0a2f35a71a185002e43fca7fe
parkermerritt <parker.merritt@gmail.com>
2017-07-12 22:23:15 -0400

    update store imgs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 <audio id="audio" src="http://freesound.org/data/previews/119/119900_1990698-lq.mp3" autostart="false"></audio>
 
+function playSound() {
+  var sound = document.getElementById("audio");
+  sound.play();
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -21,8 +25,3 @@ $(document).ready(function(){
         $(this).removeClass('transition');
     });
   });
-
-$(document).ready(function playSound() {
-  var sound = document.getElementById("audio");
-  sound.play();
-}
